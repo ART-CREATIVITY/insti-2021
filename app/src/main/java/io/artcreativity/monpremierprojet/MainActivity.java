48928514ecd37e216a0e4b207f15415876295489
Judicael ZANNOU <madrojudi.gz@gmail.com>
2021-11-04 12:52:08 +0100

    menu options

--- app/src/main/java/io/artcreativity/monpremierprojet/MainActivity.java
+++ app/src/main/java/io/artcreativity/monpremierprojet/MainActivity.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.widget.Toast;
 
@@ -13,7 +15,7 @@ import com.google.android.material.textfield.TextInputLayout;
 
 import io.artcreativity.monpremierprojet.entities.Product;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private final String TAG = MainActivity.class.getCanonicalName();
 
@@ -32,6 +34,21 @@ public class MainActivity extends AppCompatActivity {
         priceEditText = findViewById(R.id.price);
         quantityInStockEditText = findViewById(R.id.quantity_in_stock);
         alertQuantityEditText = findViewById(R.id.alert_quantity);
+//        findViewById(R.id.my_btn).setOnClickListener(new View.OnClickListener(){
+//
+//            @Override
+//            public void onClick(View view) {
+//                saveProduct(view);
+//            }
+//        });
+        findViewById(R.id.my_btn).setOnClickListener(this);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = new MenuInflater(this);
+        menuInflater.inflate(R.menu.main_menu, menu);
+        return super.onCreateOptionsMenu(menu);
     }
 
     public void saveProduct(View view) {
@@ -48,5 +65,10 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(MainActivity.this, ProductDetailActivity.class);
         intent.putExtra("MY_PROD", product);
         startActivity(intent);
+    }
+
+    @Override
+    public void onClick(View view) {
+        saveProduct(view);
     }
 }
